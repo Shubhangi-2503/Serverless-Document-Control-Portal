716a37fa7dab58456bb52ab8f711e45e3b0ca40b
Shubhangi-2503 <shubhangi0130@gmail.com>
2026-08-17 16:09:05 +0100

    Initial Commit For MVC front-end and Azure Function Backend Services

--- DocumentWebClient/DocumentWebClient/obj/Release/net10.0/DocumentWebClient.AssemblyInfo.cs
+++ DocumentWebClient/DocumentWebClient/obj/Release/net10.0/DocumentWebClient.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DocumentWebClient")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41e2e0f61b3586d16731eca00157dafcfa93f9e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("DocumentWebClient")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DocumentWebClient")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
